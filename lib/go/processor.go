075ab491b0a40313205b776fd8e55008584c71ac
Nate <Nate.Woods@workiva.com>
2018-08-17 10:39:22 -0600

    Stubbing out stupid middleware

--- lib/go/processor.go
+++ lib/go/processor.go
@@ -197,6 +197,31 @@ func (f *FBaseProcessorFunction) InvokeMethod(args []interface{}) Results {
 	return f.handler.Invoke(args)
 }
 
+/* All the cases the new middleware stuff needs to work with
+- https://github.com/Workiva/comments-service/blob/bd514f34e3cbb30e99042ff1597287fc305b7de3/server/middleware.go
+- https://github.com/Workiva/harbour-api/blob/cec3fd5cbdc63368d17d996507124a901560c7fd/server/middleware.go
+- https://github.com/Workiva/history-service/blob/78812eb620abb669344ce1ced8915f497726b360/server/middleware/middleware.go
+- https://github.com/Workiva/vessel/blob/8fb2d862df4a6f8c07702f129c3c9a916b0f0049/server/middleware/middleware.go
+- https://github.com/Workiva/content-management-api/blob/56ecbe9bf795756d8c6698f80efe805a7cae570d/cmd/data-platform-api/main.go
+- https://github.com/Workiva/app_intelligence_go/blob/a526588b6a4ed4a5b7310faa0d0c00b9b56d03cb/tracing/example/http/httpClient.go
+- https://github.com/Workiva/content-search-service/blob/2af36d3e6a3f55b76e38f0c3dad06f8a7791c130/cmd/client/main.go
+- https://github.com/Workiva/messaging-sdk/blob/7e41130c1210b6ca5ab929f26be49fa1d35519f4/lib/go/sdk/vessel_client.go
+- https://github.com/Workiva/app_intelligence_go/blob/a526588b6a4ed4a5b7310faa0d0c00b9b56d03cb/tracing/example/http/httpClient.go
+- https://github.com/Workiva/content-search-service/blob/2af36d3e6a3f55b76e38f0c3dad06f8a7791c130/cmd/client/main.go
+- https://github.com/Workiva/linking-frugal/blob/2360329898a205b1452764866f9dbf9a2a1304e0/go/middleware/iam.go
+- https://github.com/Workiva/contract_creator/blob/fb85c609e05c9b92ba46fa90068ef75b4e067d04/lib/go/contract_middleware.go
+- https://github.com/Workiva/linking-sdk/blob/3a8026fc5b20aa8b974182239b206a74b2561476/go/sdk/client.go
+- https://github.com/Workiva/messaging-sdk/blob/c345b5b27bfc55205ebaeb216a0537660a8b684f/lib/go/sdk/iam.go
+- https://github.com/Workiva/linking-frugal/blob/76d05d4e6537f990c2a31fe507f8a313e91687f6/go/middleware/robustness.go
+- https://github.com/Workiva/app_intelligence_go/blob/cce8f9c52e4da04260c91f41974e80c7c10d7ed5/tracing/middleware/messaging/messaging.go
+- https://github.com/Workiva/linking-directory/blob/5f4f7cb7fb506ef47e764b3c639a7777c881f28d/ris/ris.go
+- https://github.com/Workiva/linking-frugal/blob/c772963b675959ea9f164dad061ca542921f0764/go/middleware/paging.go
+*/
+
+type MiddlewareHandler func(service, method string, ctx FContext) error
+
+type Middleware func(MiddlewareHandler) MiddlewareHandler
+
 type ServiceDesc struct {
 	Name    string
 	Methods []MethodDesc
@@ -330,4 +355,14 @@ func (m *MethodDesc) Process(ctx FContext, iprot, oprot *FProtocol) error {
 	// 	err = err2
 	// }
 	return err
+}
+
+func storeWriteApplicationError(ctx FContext, oprot *FProtocol, type_ int32, method, message string) error {
+	x := thrift.NewTApplicationException(type_, message)
+	oprot.WriteResponseHeader(ctx)
+	oprot.WriteMessageBegin(method, thrift.EXCEPTION, 0)
+	x.Write(oprot)
+	oprot.WriteMessageEnd()
+	oprot.Flush()
+	return x
 }
