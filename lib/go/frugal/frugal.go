a7bc0e50c64d639be920c70997959f6eef0a05c9
Nate <Nate.Woods@workiva.com>
2018-08-24 10:49:26 -0600

    Adding more comments to core library

--- lib/go/frugal/frugal.go
+++ lib/go/frugal/frugal.go
@@ -1,7 +1,5 @@
 package frugal
 
-import "time"
-
 // Reader == io.Reader
 type Reader interface {
 	Read(p []byte) (n int, err error)
@@ -12,26 +10,35 @@ type Writer interface {
 	Write(p []byte) (n int, err error)
 }
 
-// CallFunc defines the generall network interface.
-type CallFunc func(ctx Context, service, method string, in Reader, out Writer) error
+// Reseter allows multiple calls to be performed to a given writer.
+// This is used for recurring notifications or pub-sub style assets.
+type Reseter interface {
+	Writer
+	Reset()
+}
 
 // A Context carries a deadline, a cancelation signal, and other values across API boundaries.
 type Context interface {
-	Deadline() (deadline time.Time, ok bool)
 	Done() <-chan struct{}
 	Err() error
 	Value(key interface{}) interface{}
 }
 
-// Error is the core error type emitted from networking methods
-type Error string
+// CallFunc defines the general networking interface.
+//
+// Iff out is a Reseter, this will act as a subscription to the service.method topic.
+// Otherwise, this will serialize in, perform an RPC and write the response to out.
+//
+type CallFunc func(ctx Context, service, method string, in Reader, out Writer) error
 
-func (e Error) Error() string { return string(e) }
-
-// NewError adds a prefix to an error
-func NewError(prefix string, err error) error {
+// Wrap adds a prefix to an error or returns nil if err was nil
+func Wrap(prefix string, err error) error {
 	if err == nil {
 		return nil
 	}
-	return Error(prefix + ": " + err.Error())
+	return frugalError(prefix + ": " + err.Error())
 }
+
+type frugalError string
+
+func (e frugalError) Error() string { return string(e) }
